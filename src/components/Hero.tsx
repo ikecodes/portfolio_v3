d0281d5c0c494d1054ee447da1290b7aee4fedfe
Onuorah Ike <ikecodes@gmail.com>
2022-05-02 23:18:59 +0100

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,14 +9,13 @@ const Hero = () => {
   return (
     <Container>
       <Header className='text-uppercase mb-0 m-0'>onuorah ikechukwu</Header>
-      <Text className='text-capitalize'>software engineer</Text>
-      <Text2 className='mt-4'>
-        I’m a software engineer specializing in building (and occasionally
-        designing) exceptional
+      <Text className='mt-4'>
+        I’m a <WhiteHighlight> software engineer</WhiteHighlight> specializing
+        in building (and occasionally designing) exceptional
         <WhiteHighlight> digital experiences</WhiteHighlight>. Currently, I’m
         focused on building accessible, human-centered products at
         <ColoredHighlight> Buzzline</ColoredHighlight>
-      </Text2>
+      </Text>
     </Container>
   );
 };
@@ -34,14 +33,8 @@ const Header = styled.h1`
     font-size: 4rem;
   }
 `;
+
 const Text = styled.p`
-  font-size: 3rem;
-  color: ${colors.white};
-  @media (max-width: 576px) {
-    font-size: 2rem;
-  }
-`;
-const Text2 = styled.p`
   color: ${colors.secondary};
   max-width: 700px;
 `;
