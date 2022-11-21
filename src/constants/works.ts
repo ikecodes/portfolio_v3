99291081f1ed7f1aed86503d06a7ea58d337ce3a
Onuorah Ike <ikecodes@gmail.com>
2022-11-21 19:00:44 +0100

    updated portfolio to just live links

--- src/constants/works.ts
+++ src/constants/works.ts
@@ -31,7 +31,8 @@ const works: {
     title: "grooming endownment trust",
     description:
       "Grooming Endowment Trust was set up as a nonprofit organisation that promotes sustainable social initiatives in education, health, enterprise and community development. Endowed with initial seed funding from Grooming Centre, the Trust provides grants and investments to social enterprises creating impact in their communities.",
-    clientUrl: "https://github.com/ikecodes/grooming_endowment_trust",
+    clientUrl: "",
+    // clientUrl: "https://github.com/ikecodes/grooming_endowment_trust",
     serverUrl: "",
     liveUrl: "https://www.groomingendowment.org",
     openSource: true,
@@ -55,7 +56,8 @@ const works: {
     description:
       "Xaron technologies solves delievery services problem in Nigeria. A system created for delivery service owners to easily manage there riders. Verified independent riders can also signup and have access to these features. Xaron helps users find delivery riders nearest to there location.",
     clientUrl: "",
-    serverUrl: "https://github.com/ikecodes/xaron_backend",
+    serverUrl: "",
+    // serverUrl: "https://github.com/ikecodes/xaron_backend",
     liveUrl: "https://apps.apple.com/ng/app/xaron/id1624015119",
     openSource: false,
     stacks: ["React", "Bootstrap", "Socket.io", "Node.js/Express", "MongoDb"],
@@ -65,8 +67,10 @@ const works: {
     title: "broadcast nigeria",
     description:
       "E-commerce web application for the sale of various sound equipments.",
-    clientUrl: "https://github.com/ikecodes/broadcast-nigeria_frontend",
-    serverUrl: "https://github.com/ikecodes/broadcast-nigeria_backend",
+    clientUrl: "",
+    // clientUrl: "https://github.com/ikecodes/broadcast-nigeria_frontend",
+    serverUrl: "",
+    // serverUrl: "https://github.com/ikecodes/broadcast-nigeria_backend",
     liveUrl: "https://broadcastnigeria.vercel.app/",
     openSource: true,
     stacks: ["React", "Redux", "CSS modules", "Node.js/Express", "MongoDb"],
@@ -87,7 +91,8 @@ const works: {
     title: "Ecomms",
     description:
       "Store locator web application that lets you easily find stores and products based on you location or one you set. Its an ecommerce web app with a store locator.",
-    clientUrl: "https://github.com/ikecodes/ecommerce",
+    clientUrl: "",
+    // clientUrl: "https://github.com/ikecodes/ecommerce",
     serverUrl: "",
     liveUrl: "https://ecomms.vercel.app/",
     openSource: true,
